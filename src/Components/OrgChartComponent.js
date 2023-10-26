6b2f9c309e406ee3d460b33900654df677db4eb5
PrasannaVigneswari <146935812+PrasannaVigneswari@users.noreply.github.com>
2023-10-26 14:53:17 +0530

    Update OrgChartComponent.js

--- src/Components/OrgChartComponent.js
+++ src/Components/OrgChartComponent.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Tree, TreeNode } from "react-organizational-chart";
 import styled from "@emotion/styled";
-
-import "./OrgChart.css";
+import "./OrgChartComponents.css";
 const StyledNode = styled.div`
   padding: 8px;
   border-radius: 8px;
